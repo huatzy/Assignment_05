94350fd14ac1ab3c511e8666d9e9637f0ff332d2
David-0424 <56601563+David-0424@users.noreply.github.com>
2019-11-24 17:57:11 +0800

    Create Complex.h

---
+++ 201816040105/Ex10_08/Complex.h
@@ -0,0 +1,22 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+#include<iostream>
+class Complex
+{
+	friend std::ostream &operator<<(std::ostream &, const Complex &);
+	friend std::istream &operator>>(std::istream &, Complex &);
+	public:
+		explicit Complex( double=0.0,double=0.0);
+		Complex operator+( const Complex & ) const;
+		Complex operator-( const Complex & ) const;
+		Complex operator*( const Complex & ) const;
+		bool operator==( const Complex &) const;
+		bool operator!=( const Complex &right) const
+		{
+			return ! (*this==right);
+		}
+	private:
+		double real;
+		double imaginary;
+};
+#endif
